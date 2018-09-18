668082a3b02556a5e5fb1fefee149ed9e246d469
JonathanThomaz <jonathanthomaz96@gmail.com>
2018-09-18 10:29:37 -0300

    Add alterar UsuarioDAO

--- src/dao/UsuarioDAO.java
+++ src/dao/UsuarioDAO.java
@@ -1,24 +1,24 @@
 public class UsuarioDAO{
 
-    public static void inserir(Usuario usuario) throws SQLException, classNotFoundExceptio(
-            Connection conexao = null;
-            PreparedStatemet comando = null;
-            try {
-                conexao = BD.getConexao();
-                String sql = "INSERT INTO usuario (id , nome, email, senha, tipoUsario) VALUES (?,?,?,?,?)";
-                comando = conexao.prepareStatemant(sql);
-                comando.setLong(1, usuario.getId());
-                comando.setString(2, usuario.getNome());
-                comando.setString(3, usuario.getEmail());
-                comando.setString(4, usuario.getSenha());
-                comando.setString(5, usuariogetTipoUsuario());
+    public static void inserir(Usuario usuario) throws SQLException, classNotFoundExceptio {
+        Connection conexao = null;
+        PreparedStatemet comando = null;
+        try {
+            conexao = BD.getConexao();
+            String sql = "INSERT INTO usuario (id , nome, email, senha, tipoUsario) VALUES (?,?,?,?,?)";
+            comando = conexao.prepareStatemant(sql);
+            comando.setLong(1, usuario.getId());
+            comando.setString(2, usuario.getNome());
+            comando.setString(3, usuario.getEmail());
+            comando.setString(4, usuario.getSenha());
+            comando.setString(5, usuariogetTipoUsuario());
 
-                comando.execute();
-                BD.fecharConexao(conexao, comando);
-            } catch (SQLException e){
-                throw e;
-            }
-            )
+            comando.execute();
+            BD.fecharConexao(conexao, comando);
+        } catch (SQLException e) {
+            throw e;
+        }
+    }
     }
 
     public static void alterar(Usuario usuario) throws SQLException, classNotFoundException{
@@ -33,5 +33,9 @@ public class UsuarioDAO{
                 comando.setString(3, usuario.getSenha());
                 comando.setString(4, usuario.getTipoUsuario());
                 comando.setLong (5, usuario.getId());
+                comando.execute();
+                BD.fecharConexao(conexao, comando);
+            }catch (SQLException e){
+                throw e;
             }
     }
