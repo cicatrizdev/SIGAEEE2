4ab48310455fa74866b4a4204915291a30f05b53
cicatrizwp <pedrocmello.bsi@gmail.com>
2018-09-18 08:59:32 -0300

    dao

--- src/dao/BD.java
+++ src/dao/BD.java
@@ -1,4 +1,16 @@
 package dao;
 
-public class BD {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException
+
+public abstract class BD {
+
+    public static Connection getConexao()
+        throws ClassNotFoundException, SQLException{
+        Connection conexao = null;
+        Class.forName("com.mysql.jdbc.Driver");
+        return DriverManager.getConnection
+                ("jdbc:mysql://localhost/sca", "root", "");
+    }
 }
