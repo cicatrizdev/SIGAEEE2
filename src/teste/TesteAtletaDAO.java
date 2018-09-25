b7937e2a3336abb0c445a1e7b52d20b5819917d3
cicatrizwp <pedrocmello.bsi@gmail.com>
2018-09-25 09:23:43 -0300

    erro

---
+++ src/teste/TesteAtletaDAO.java
@@ -0,0 +1,11 @@
+package teste;
+
+import dao.AtletaDAO;
+import model.Atleta;
+
+public class TesteAtletaDAO {
+    public static void main(String[] args) {
+        Atleta gordao = new Atleta();
+        AtletaDAO.inserir;
+    }
+}
